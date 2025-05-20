f540ad10258b3f96574fc2d5bd3b395ead477ffe
lucax13 <lucas13lima2006@gmail.com>
2025-05-20 09:34:13 -0300

    controle de acesso

---
+++ src/Auth/ControDeAcesso.php
@@ -0,0 +1,12 @@
+<?php
+namespace Microblog\Auth;
+/*sobre sessões no php
+sessão (session) é uma funcionalidade usada para o controle
+de acesso  e outras informações que sejam importantes enquanto
+o navegador estiver aberto com o site.
+
+Exemplos: áreas administrativas, painel de controle/dashboar,
+aréa do cliente, área do aluno etc.
+
+nestas áreas o acesso se dá através de alguma forma de autenticação.
+exemplos: login/senha, biometria, facial, 2-fatores etc.*/
