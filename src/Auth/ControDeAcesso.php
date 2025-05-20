330e9def053ffd95381322c5552d2f3ae4ae4f35
lucax13 <lucas13lima2006@gmail.com>
2025-05-20 09:52:43 -0300

    adiciona iniciarsessão e exigirLogin

--- src/Auth/ControDeAcesso.php
+++ src/Auth/ControDeAcesso.php
@@ -10,3 +10,28 @@ aréa do cliente, área do aluno etc.
 
 nestas áreas o acesso se dá através de alguma forma de autenticação.
 exemplos: login/senha, biometria, facial, 2-fatores etc.*/
+
+final class ControleDeAcesso 
+{
+    private function __construct() {}
+
+    /*Iniciar sessão caso na tenha sessão */
+    private static function iniciarSessao():void
+    {
+        if(!isset($_SESSION)) session_start();
+    }
+
+    /*"bloqueia" páginas admin caso o usuario não esteja logado*/
+    public function exigirLogin(): void
+    {
+        //inicia sessão se necessario
+        self::iniciarSessao();
+
+        //Se não existir uma variavel de sessão chamada ID
+        if(!isset($_SESSION['id'])){
+            session_destroy();
+            header("location:../login.php?acesso_proibido");
+            exit;
+        }
+    }
+}
